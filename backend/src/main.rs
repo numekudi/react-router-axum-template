ed13553b4c1541ec476d0ff63e33e82dec0c8894
numekudi <t.n.mailaddmain@gmail.com>
2025-01-01 11:47:36 +0900

    Add Aide and Schemars dependencies; implement OpenAPI support in Axum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,36 +1,65 @@
 use std::env;
 
+use aide::{
+    axum::{routing::get, ApiRouter, IntoApiResponse},
+    openapi::{Info, OpenApi},
+};
 use axum::{
     http::{HeaderValue, Method},
-    response,
-    routing::get,
-    Router,
+    Extension, Json,
 };
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
+
+use schemars::JsonSchema;
 use tower_http::cors::{Any, CorsLayer};
-#[derive(Deserialize, Serialize)]
+
+#[derive(Deserialize, Serialize, JsonSchema)]
 struct Sample {
     data: String,
 }
 
-async fn root_handler() -> response::Json<Sample> {
-    response::Json(Sample {
+async fn root_handler() -> impl IntoApiResponse {
+    Json(Sample {
         data: "Hello World".to_string(),
     })
+}
+
+async fn serve_api(Extension(api): Extension<OpenApi>) -> impl IntoApiResponse {
+    Json(api)
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+    dbg!(env::var("ORIGIN").unwrap());
     let allow_origin = env::var("ORIGIN").unwrap();
-    let app = Router::new().route("/", get(root_handler)).layer(
-        CorsLayer::new()
-            .allow_origin(allow_origin.parse::<HeaderValue>().unwrap())
-            .allow_methods([Method::GET, Method::POST])
-            .allow_headers(Any),
-    );
+    let app = ApiRouter::new()
+        .api_route("/hello", get(root_handler))
+        .route("/api.json", get(serve_api))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(allow_origin.parse::<HeaderValue>().unwrap())
+                .allow_methods([Method::GET, Method::POST])
+                .allow_headers(Any),
+        );
+
+    let mut api = OpenApi {
+        info: Info {
+            description: Some("an example API".to_string()),
+            ..Info::default()
+        },
+        ..OpenApi::default()
+    };
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(
+        listener,
+        app.finish_api(&mut api)
+            // Expose the documentation to the handlers.
+            .layer(Extension(api))
+            .into_make_service(),
+    )
+    .await
+    .unwrap();
 }
